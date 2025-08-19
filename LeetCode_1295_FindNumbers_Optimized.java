a5ed92de62287468400c9a2d7097617164be6951
francisglenpereira <glenpereira2004@gmail.com>
2025-08-19 22:10:12 +0530

    Update LeetCode_1295_FindNumbers_Optimized.java
    fixed typo

--- LeetCode_1295_FindNumbers_Optimized.java
+++ LeetCode_1295_FindNumbers_Optimized.java
@@ -1,25 +1,11 @@
-// Version 1: Using Math.log10
-class SolutionLog10 {
+class Solution {
     public int findNumbers(int[] nums) {
-        int count = 0;
-        for (int num : nums) {
-            int digits = (int)Math.log10(num) + 1;
-            if (digits % 2 == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
-}
-
-// Version 2: Optimized using range checks
-class SolutionOptimized {
-    public int findNumbers(int[] nums) {
-        int count = 0;
-        for (int num : nums) {
-            if ((num >= 10 && num <= 99) || 
-                (num >= 1000 && num <= 9999) || 
-                num == 100000) {
+        int count =0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int digits = (int)Math.log10(nums[i])+1;
+            if(digits%2==0)
+            {
                 count++;
             }
         }
